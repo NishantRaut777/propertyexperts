0b49af95831769ee483c20e1b43783d9f9f16c41
Nishant <nishraut7@gmail.com>
2023-10-27 00:37:40 +0530

    added show user listings functionality.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -25,6 +25,11 @@ const Profile = () => {
   const [formData, setFormData] = useState({});
 
   const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const [showListingsError, setShowListingsError] = useState(false);
+
+  // to save user's listings
+  const [userListings, setUserListings] = useState([]);
   
   // manipulating user state
   const dispatch = useDispatch();
@@ -142,78 +147,166 @@ const Profile = () => {
     }
   };
 
-  
+  // handling show Listing
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+
+      if (data.success === false){
+        setShowListingsError(true);
+        return;
+      }
+
+      // on successful retrival store the listings data in state variable for further process
+      setUserListings(data);
+
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form  onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input 
-        onChange={(e) => setFile(e.target.files[0])}
-        type='file' ref={fileRef} hidden accept='image/*' />
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
 
         {/* If formdata has image then show it else show current user image */}
-        <img onClick={() => fileRef.current.click()}  src={formData.avatar || currentUser.avatar} alt='profile'
-          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
 
-        <p className='text-sm self-center'>
-          { fileUploadError ? (
-            <span className='text-red-700'>Error Image upload (image must be less than 2mb).</span>
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">
+              Error Image upload (image must be less than 2mb).
+            </span>
           ) : fileUploadPercent > 0 && fileUploadPercent < 100 ? (
-            <span className='text-slate-700'>
+            <span className="text-slate-700">
               {`Uploading ${fileUploadPercent}%`}
             </span>
-          ) : fileUploadPercent === 100  ? (
-            <span className='text-green-700'>
-              Image successfully uploaded!
-            </span>
+          ) : fileUploadPercent === 100 ? (
+            <span className="text-green-700">Image successfully uploaded!</span>
           ) : (
             ""
-          )
-        }
+          )}
         </p>
 
-        <input type='text' placeholder='username' id='username'
-        defaultValue={currentUser.username}
-        onChange={handleChange}
-         className='border p-3 rounded-lg' />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          defaultValue={currentUser.username}
+          onChange={handleChange}
+          className="border p-3 rounded-lg"
+        />
 
-        <input type='email' placeholder='email' id='email'
-        defaultValue={currentUser.email}
-        onChange={handleChange}
-         className='border p-3 rounded-lg' />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
+          className="border p-3 rounded-lg"
+        />
 
-        <input type='password' placeholder='password' id='password'
-        onChange={handleChange}
-         className='border p-3 rounded-lg' />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="border p-3 rounded-lg"
+        />
 
-         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opcaity-95'>
-           { loading ? "Loading..." : "Update" }
-         </button>
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opcaity-95"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
 
-         <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
-            Create Listing
-         </Link>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
-        <span 
-        onClick={handleDeleteUser}
-        className='text-red-700 cursor-pointer'>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
           Delete Account
         </span>
-        <span 
-        onClick={handleSignOut}
-        className='text-red-700 cursor-pointer'>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
           Sign out
         </span>
       </div>
 
-      <p className='text-red-700 mt-5 text-center' >{ error ? error : "" }</p>
+      <p className="text-red-700 mt-5 text-center">{error ? error : ""}</p>
 
-      <p className='text-green-700 mt-5 text-center'>{updateSuccess ? "User is updated successfully": ""}</p>
+      <p className="text-green-700 mt-5 text-center">
+        {updateSuccess ? "User is updated successfully" : ""}
+      </p>
+
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
+      
+
+      {/* If userListings has the data then map through it and display all listings */}
+      {userListings && userListings.length > 0 && 
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing image"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+
+              <Link
+                to={`/listing/${listing._id}`}
+                className="text-slate-700 font-semibold hover:underline truncate flex-1"
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      }
     </div>
-  )
+  );
 }
 
 export default Profile
