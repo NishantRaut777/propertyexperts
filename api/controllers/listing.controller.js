aca42d4516b7d72d3517c39b9a308fc7bcac2161
Nishant <nishraut7@gmail.com>
2023-10-27 12:44:22 +0530

    added update listings api route.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -32,3 +32,30 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 };
+
+
+// UPDATE LISTING
+export const updateListing = async (req, res, next) => {
+    // First check if listing exists or not
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, "Listing not found"));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, "You can only update your own listings"));
+    }
+
+    try {
+        const updatedListings = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListings);
+
+    } catch (error) {
+        next(error);
+    }
+};
