aefc33fb5583cbc3f78546e8fc87b5b7480f3ed7
Nishant <nishraut7@gmail.com>
2023-10-28 17:38:43 +0530

    added search listings by query api route.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -75,3 +75,62 @@ export const getListing = async (req, res, next) => {
         next(error);
     }
 };
+
+
+// GET LISTINGS BY QUERY
+export const getListings = async (req, res, next) => {
+    try {
+        // set limit value to the values passed else set it 9
+        const limit = parseInt(req.query.limit) || 9;
+
+        const startIndex =  parseInt(req.query.startIndex) || 0;
+
+        let offer = req.query.offer;
+
+        // if offer is undefined or false show listings both having offers and no offers
+        if (offer === undefined || offer === "false"){
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+
+        if (furnished === undefined || furnished === "false"){
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+
+        if (parking === undefined || parking === "false"){
+            parking = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+
+        // if type is undefined or all then give listings having both sale and rent
+        if (type === undefined || type === "all"){
+            type = { $in: ["sale", "rent"] };
+        }
+
+        const searchTerm = req.query.searchTerm || "";
+
+        const sort = req.query.sort || "createdAt";
+
+        const order = req.query.order || "desc";
+
+        // Search with above parameters
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: "i" },
+            offer,
+            furnished,
+            parking,
+            type
+        }).sort(
+            {[sort]: order}
+        ).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing, deleteListing, getListing, updateListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getListing, updateListing, getListings } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.put("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+
+// get listings by query
+router.get("/get", getListings);
 
 export default router;
